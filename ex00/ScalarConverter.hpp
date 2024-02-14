d994663395bc5c1f2b80c4f882875de79f8d6023
Ivo Marques <110631286+Pastilhex@users.noreply.github.com>
2024-02-14 07:38:34 +0000

    Starting with class definition

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialves-m <ialves-m@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 06:45:17 by ialves-m          #+#    #+#             */
+/*   Updated: 2024/02/14 07:37:55 by ialves-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _SCALAR_
+# define _SCALAR_
+
+#include <iostream>
+
+class ScalarConverter {
+public:
+    ScalarConverter();
+    ~ScalarConverter();
+    ScalarConverter(const ScalarConverter&);
+    ScalarConverter& operator=(const ScalarConverter&);
+    void convert(const std::string);
+};
+
+#endif
